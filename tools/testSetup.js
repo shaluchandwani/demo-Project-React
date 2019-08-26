d19d474afc676919a0f153fceb95937a4d45a764
shaluchandwani <51748364+shaluchandwani@users.noreply.github.com>
2019-08-26 08:42:40 +0200

    Update package.json

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -1,3 +1,4 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
